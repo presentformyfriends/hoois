3be9f7c20353b260e6d0bffe1c4e987631727d43
presentformyfriends <devmike942@gmail.com>
2023-03-09 15:42:28 -0500

    Update settings for deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '159.203.39.247', user: 'deploy', roles: %w[app web]
 
 # role-based syntax
 # ==================
